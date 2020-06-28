3d24a7f987eb1f8cbcd27d785b0bedc367b1ae72
devWRM <mcwen@comcast.net>
2020-06-28 09:14:23 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,21 @@
+require "pry"
+
+class Song
+    attr_accessor :name, :artist
+    @@all = []
+
+    def initialize(name)
+        @name = name
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    
+
+
+
+end
