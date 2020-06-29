42e6f0b7b20ee7e90b215246ac4e52bd1151f471
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-29 19:11:07 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -3,14 +3,23 @@ class Artist
   attr_accessor :name, :songs
 
   @@all = []
-  
+    
   def initialize(name)
     @name = name 
   end 
   
-  def self.all 
-     @@all 
-  end 
+  # def self.all 
+  #   @@all 
+  # end 
   
+  # def add_song(song)
+  #   @songs << song
+  # end 
+  
+  # def self.find_or_create_by_name
+  # end 
+  
+  # def print_songs
+    
   
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -2,6 +2,8 @@ class Song
   
   attr_accessor :artist, :name
   
+  @@all = []
+  
   def initialize(name)
     @name = name 
   end 
@@ -11,15 +13,16 @@ class Song
   end 
   
   def self.new_by_filename
-    songinfo
+    song_name = file.split(" ")[1]
+    artist = file.split(" ")[0]
+    song = self.new(song_name)
+    song.artist_name = artist 
+    song
   end 
   
   def artist_name
     self.artist = Artist.find_or_create_by_name(name)
-    artist.add_song(self)
+    self.artist.add_song(self)
   end 
-  
-  
-  
   
 end 
