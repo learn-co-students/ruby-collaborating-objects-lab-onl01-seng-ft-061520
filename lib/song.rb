e303953d2edb109197eb5409fa1ed037ca1fd3d2
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-30 15:42:06 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -14,15 +14,19 @@ class Song
   end 
   
   def self.new_by_filename(file)
-    song_name = file.split(" - ")[1]
-    artist = file.split(" - ")[0]
-    song = self.new(song_name)
-    song.artist_name = artist 
-    song 
-  end
+        mp3_deleted = file.split(/.mp3/).join(" ")
+        names_and_genre = mp3_deleted.split(/ - /)
+            artist_name = names_and_genre[0]
+            song_name = names_and_genre[1]
+        
+
+        new_song = self.new(song_name)
+        new_song.artist = Artist.find_or_create_by_name(artist_name)
+        new_song
+    end
   
   def artist_name=(artist_name)
-    self.artist = Artist.find_or_create_by_name(name)
+    self.artist = Artist.find_or_create_by_name(artist_name)
   end 
   
 end 
