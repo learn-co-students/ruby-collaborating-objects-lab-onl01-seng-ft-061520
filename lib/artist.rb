391bd80b8f6326586bca1b349589029f5a615b6d
Shirlen Detablan <slen.detablan@gmail.com>
2020-06-30 15:25:39 +0000

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -25,14 +25,8 @@ class Artist
     self.all.find{|artist| artist.name == artist_name}
   end 
     
-  def self.find_or_create_by_name
-    if find_artist(artist_name) == nil
-      new_artist = self.new(artist_name)
-      new_artist
-    else 
-      find_artist(artist_name)
-    end
-  end 
+  # def self.find_or_create_by_name
+  # end 
     
   def print_songs
     songs.map{|song| puts song.name}
--- lib/song.rb
+++ lib/song.rb
@@ -13,16 +13,9 @@ class Song
     @@all 
   end 
   
-  def self.new_by_filename
-    mp3_deleted = file.split(/.mp3/).join(" ")
-    names_and_genre = mp3_deleted.split(/ - /)
-    artist_name = names_and_genre[0]
-    song_name = names_and_genre[1]
-    new_song = self.new(song_name)
-    new_song.artist = Artist.find_or_create_by_name(artist_name)
-    new_song
+  # def self.new_by_filename
     
-  end
+  # end
   
   def artist_name
     self.artist = Artist.find_or_create_by_name(name)
